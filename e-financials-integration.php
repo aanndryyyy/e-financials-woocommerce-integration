0064f12ddc28391bc86b5d312cedc1722f0b03fa
Andry Pedak <andry.pedak@gmail.com>
2023-02-22 21:35:21 +0000

    Initialize project

---
+++ e-financials-integration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: e-Financials WooCommerce Intergration
+ * Plugin URI: https://github.com/aanndryyyy/e-financials-woocommerce-integration
+ * Description: WooCommerce e-Financials integration for easy bookkeeping (E-arveldaja WooCommerce liidestus). 
+ * Requires at least: 6.0
+ * Requires PHP: 8.0
+ * Version: 0.0.1
+ * Text Domain: e-financials
+ */
