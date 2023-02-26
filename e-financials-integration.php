4feb85807cf51ed4f7276d7f273f5245bbc4d4fe
Andry Pedak <andry.pedak@gmail.com>
2023-02-26 10:34:21 +0000

    Add WC requirements

--- e-financials-integration.php
+++ e-financials-integration.php
@@ -3,8 +3,12 @@
  * Plugin Name: e-Financials WooCommerce Intergration
  * Plugin URI: https://github.com/aanndryyyy/e-financials-woocommerce-integration
  * Description: WooCommerce e-Financials integration for easy bookkeeping (E-arveldaja WooCommerce liidestus). 
+ * Version: 0.0.1
+ *
  * Requires at least: 6.0
  * Requires PHP: 8.0
- * Version: 0.0.1
+ * WC requires at least: 7.0
+ * WC tested up to: 7.4
+ *
  * Text Domain: e-financials
  */
